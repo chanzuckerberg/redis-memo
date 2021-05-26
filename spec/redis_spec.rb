52661958f482774bcc5aeaa4097fd6f40bad7b05
Katy Ho <kho@chanzuckerberg.com>
2021-05-26 11:28:34 -0700

    Add test case for raising error in evalsha not covered in codecov

--- spec/redis_spec.rb
+++ spec/redis_spec.rb
@@ -64,24 +64,34 @@ describe RedisMemo::Redis do
     expect(client.mapped_mget('a').is_a?(Hash)).to be(true)
   end
 
-  it 'calls evalsha after a script is already loaded' do
-    client = RedisMemo::Redis.new
-    client.script(:flush)
-    script = <<~LUA
-      redis.call('set', KEYS[1], ARGV[1])
-    LUA
+  context 'run_script' do
+    let(:lua_script) { "redis.call('set', KEYS[1], ARGV[1])" }
+    let(:script_sha) { Digest::SHA1.hexdigest(lua_script) }
+    let(:client) { RedisMemo::Redis.new }
 
-    # When the script sha isn't loaded on Redis, it should fall back to calling eval
-    script_sha = Digest::SHA1.hexdigest(script)
-    expect(client).to receive(:evalsha).once.and_call_original
-    expect(client).to receive(:eval).once.and_call_original
-    client.run_script(script, script_sha, keys: ['cache_key'], argv: ['cache_value'])
+    before(:each) do
+      client.script(:flush)
+    end
 
-    # Subsequent calls should use evalsha
-    5.times do
-      expect(client).to receive(:evalsha).and_call_original
-      expect(client).to_not receive(:eval)
-      client.run_script(script, script_sha, keys: ['cache_key'], argv: ['cache_value'])
+    it 'calls evalsha after a script is already loaded' do
+      # When the script sha isn't loaded on Redis, it should fall back to calling eval
+      expect(client).to receive(:evalsha).once.and_call_original
+      expect(client).to receive(:eval).once.and_call_original
+      client.run_script(lua_script, script_sha, keys: ['cache_key'], argv: ['cache_value'])
+
+      # Subsequent calls should use evalsha
+      5.times do
+        expect(client).to receive(:evalsha).and_call_original
+        expect(client).to_not receive(:eval)
+        client.run_script(lua_script, script_sha, keys: ['cache_key'], argv: ['cache_value'])
+      end
+    end
+
+    it 'only rescues NOSCRIPT errors from redis' do
+      allow(client).to receive(:evalsha).and_raise(Redis::CommandError)
+      expect {
+        client.run_script(lua_script, script_sha)
+      }.to raise_error(Redis::CommandError)
     end
   end
 end
